95a15a9f8acad61abcac13888790b5994be76e29
Sriramkgdms <70965789+Sriramkgdms@users.noreply.github.com>
2020-09-08 22:35:50 +0530

    new

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,53 @@
-  var $ajaxload_popup = $('.ajaxload-popup');
-            if( $ajaxload_popup.length > 0 ) {
-                $ajaxload_popup.magnificPopup({
-                  type: 'ajax',
-                  alignTop: true,
-                  overflowY: 'scroll', // as we know that popup content is tall we set scroll overflow by default to avoid jump
-                  callbacks: {
-                    parseAjax: function(mfpResponse) {
-                      THEMEMASCOT.initialize.TM_datePicker();
-                      THEMEMASCOT.initialize.TM_sliderRange();
-                      THEMEMASCOT.initialize.TM_ddslick();
-                    }
-                  }
-                });
-            }
+(function ($) {
+
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets    
+    });
+
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+          } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+          }
+    });
+
+
+    // PARALLAX EFFECT
+    $.stellar({
+      horizontalScrolling: false,
+    }); 
+
+
+    // ABOUT SLIDER
+    $('.owl-carousel').owlCarousel({
+      animateOut: 'fadeOut',
+      items: 1,
+      loop: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+    });
+
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.custom-navbar a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    });  
+
+})(jQuery);
